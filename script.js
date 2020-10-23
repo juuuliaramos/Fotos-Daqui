2104074a14ae70db14d8d847d0bcecbc85cfe519
Juĺia Sperandio <juhsperandio@hotmail.com>
2020-10-23 08:34:13 -0300

    alterei as functions next, previous e error e fiz  mobile

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ let longitude = 0;
 let index = 0;
 
 function geoFindMe() {
+
   let input = document.getElementById("input");
   let textArea = input.value;
-  console.log(textArea);
+  //console.log(textArea);
+
   //pega a localização e chama a função que faz o fetch()
   function success(position) {
     latitude = position.coords.latitude;
@@ -19,7 +21,8 @@ function geoFindMe() {
     flickr(latitude, longitude, textArea);
   }
 
-  //pelo fetch envia para o link e recebe a promise. Depois já cria o array com os link e pega uma imagem
+  //pelo fetch envia para o link e recebe a promise. 
+  //Depois já cria o array com os link e pega uma imagem
   function flickr(latitude, longitude, textArea) {
     fetch(
       `https://www.flickr.com/services/rest/?api_key=ba599f6e09e26bb416823156b39066b9&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${latitude}lon=${longitude}&text=${textArea}`
@@ -27,7 +30,7 @@ function geoFindMe() {
       .then((result) => result.json())
       .then((param) => {
         let myArr = param.photos.photo;
-        console.log(myArr);
+        //console.log(myArr);
         let res = [];
         myArr.map((x) => {
           let i = constructImageURL(x);
@@ -41,6 +44,8 @@ function geoFindMe() {
 
   function error() {
     status.textContent = "Unable to retrieve your location";
+    latitude = 36.4766;
+    longitude = -95.019236;
   }
 
   //constrói a URL
@@ -57,10 +62,9 @@ function geoFindMe() {
       ".jpg"
     );
   }
-  //seta latitude e longitude se o usuário não liberar ou chama a função success
+
   if (!navigator.geolocation) {
-    latitude = 36.4766;
-    longitude = -95.019236;
+       error()
   } else {
     status.textContent = "Locating…";
     navigator.geolocation.getCurrentPosition(success, error);
@@ -72,11 +76,19 @@ function prev() {
     index -= 1;
     geoFindMe();
   }
+ else{
+    index = 5;
+    geoFindMe();
+  }
 }
 
 function next() {
   if (index < 5) {
     index += 1;
+    geoFindMe();
+  }
+  else {
+    index = 1;
     geoFindMe();
   }
 }
